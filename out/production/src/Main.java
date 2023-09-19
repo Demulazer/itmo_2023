58858c7f789497b3235403785670c5a968f3ad46
Demulazer <79304118+Demulazer@users.noreply.github.com>
2023-09-19 15:27:51 +0300

    Add files via upload

---
+++ out/production/src/Main.java
@@ -0,0 +1,19 @@
+
+
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner sin = new Scanner(System.in);
+        int a1 = sin.nextInt(),b1 = sin.nextInt(),c1 = sin.nextInt(); // цена товара
+        int a2 = sin.nextInt(),b2 = sin.nextInt(),c2 = sin.nextInt(); // сумма чела
+        int sumDif = (((a2*12)+b2)*20+c2) - (((a1*12)+b1)*20+c1);
+        System.out.println(sumDif > 0 ?
+                (sumDif / 240) + " фунт " + ((sumDif % 240) / 20) + " шиллинг " + (((sumDif % 240 )% 20) ) + " пенс "
+                :(sumDif == 0  ?
+                "Сдачи нет"
+                : "Ошибка"));
+    }
+}
